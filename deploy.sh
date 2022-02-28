37e3c53d8bf8e028a580d4ab8c5afca3065900d8
Serkan KONAKCI <serkankonakci@gmail.com>
2022-02-28 23:02:35 +0300

    refactor: deploy path fix

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com:serkon/movime.git master:gh-pages
+git push -f git@github.com:serkon/muvime.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
   transpileDependencies: true,
-  publicPath: '/movime/'
+  publicPath: '/muvime/'
 }
